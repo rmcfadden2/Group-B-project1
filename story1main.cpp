39138ef801f8e1a20b684254de11873bd57be4cf
Jerry3570 <gongjie0357@gmail.com>
2025-09-15 16:51:18 -0700

    Create story1main.cpp
    the main implementation of story 1 but output to terminal instead of UI

---
+++ story1main.cpp
@@ -0,0 +1,33 @@
+//implementation for story1
+#include "story1.h"
+
+int main()
+{
+    //initialize the cities
+    vector<City> cities;
+    cities.emplace_back("Amsterdam");
+    cities.emplace_back("Berlin");
+    cities.emplace_back("Budapest");
+    cities.emplace_back("Brussels");
+    cities.emplace_back("Hamburg");
+    cities.emplace_back("Lisbon");
+    cities.emplace_back("London");
+    cities.emplace_back("Madrid");
+    cities.emplace_back("Paris");
+    cities.emplace_back("Prague");
+    cities.emplace_back("Rome");
+    loadDistances("distance.txt", cities);
+
+    //only output the distance from berlin 
+    for (const auto& city : cities) 
+    {
+        if(city.getName() == "Berlin")
+        {
+            city.printDistances();
+            cout << "---------------------------\n";
+        }
+    }
+    
+    return 0;
+
+}
