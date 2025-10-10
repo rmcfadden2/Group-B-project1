96ae47c96842a77a711d3248fa71d582d39536b4
Etvnnn-Sanchez <etvnsan@gmail.com>
2025-10-10 16:48:38 -0700

    Update popularFood.cpp
    adding an admin function to set new prices

--- popularFood.cpp
+++ popularFood.cpp
@@ -4,3 +4,7 @@
 PopularFood::PopularFood(const std::string& n, double p) : name(n), price(p) {}
 std::string PopularFood::getName() const { return name; }
 double PopularFood::getPrice() const { return price; }
+// New Admin Setter
+void PopularFood::setPrice(double newPrice) {
+    price = newPrice;
+}
